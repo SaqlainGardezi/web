1ab0c581f4e35173b5b4d2422d4603249ea0d3e8
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2017-03-14 12:23:41 +0500

    MediaStream

--- public/client.js
+++ public/client.js
@@ -1,3 +1,5 @@
+var stream;
+
 function hasUserMedia(){
 	navigator.getUserMedia=navigator.getUserMedia || navigator.webkitGetUserMedia || 
 	navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -8,9 +10,11 @@ if (hasUserMedia) {
 	navigator.getUserMedia=navigator.getUserMedia || navigator.webkitGetUserMedia || 
 	navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-	navigator.getUserMedia({video:true, audio:true}, function(stream){
+	navigator.getUserMedia({video:true, audio:true}, function(s){
+		stream=s;
 		var video=document.querySelector('video');
-		console.log(stream);
+	
+
 		video.src=window.URL.createObjectURL(stream);
 	},
 	function(err){
@@ -21,3 +25,33 @@ if (hasUserMedia) {
 else{
 	alert("Error. WebRTC is not supported");
 }
+
+btnGetAudioTracks.addEventListener("click", function(){
+	console.log("getAudioTracks");
+	console.log(stream.getAudioTracks());
+});
+
+btnGetTrackById.addEventListener("click", function(){
+	console.log("getTrackById");
+	console.log(stream.getTrackById(stream.getAudioTracks()[0].id));
+});
+
+btnGetTracks.addEventListener("click", function(){
+	console.log("getTracks()");
+	console.log(stream.getTracks());
+});
+
+btnGetVideoTracks.addEventListener("click", function(){
+	console.log("getVideoTracks()");
+	console.log(stream.getVideoTracks());
+});
+
+btnRemoveAudioTracks.addEventListener("click", function(){
+	console.log("removeAudioTracks()");
+	stream.removeTrack(stream.getAudioTracks()[0]);
+});
+
+btnRemoveVideoTracks.addEventListener("click", function(){
+	console.log("removeVideoTrack()");
+	stream.removeTrack(stream.getVideoTracks()[0]);
+});
